edfcd72897a3318c3163907a8bd3ac3e0b00ce1a
linyoumu <848516118@qq.com>
2023-04-04 23:46:44 +0800

    代码提交

--- SwiftNative/ViewController.swift
+++ SwiftNative/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hhh")
+        print("kkk")
         
         // Do any additional setup after loading the view.
     }
