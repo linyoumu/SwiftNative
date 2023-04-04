fcadeed7da4140777b1890becdddc3c1c4365eaa
LinYoumu <linyoumu@zc-campus.com>
2023-04-04 23:04:39 +0800

    初始化项目

---
+++ SwiftNative/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftNative
+//
+//  Created by LinMacmini on 2023/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hhh")
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
